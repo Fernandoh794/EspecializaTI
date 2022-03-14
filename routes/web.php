df0ca161486a6ca244f551f76236b5c255b7257f
Fernandoh794 <cara-se@hotmail.com>
2022-03-14 19:25:28 -0300

    First

---
+++ routes/web.php
@@ -0,0 +1,12 @@
+<?php
+use App\Http\Controllers\UserController;
+use Illuminate\Support\Facades\Route;
+
+
+Route::get('/user', [UserController::class, 'index'])->name('users.index');
+Route::get('/user/{id}', [UserController::class, 'show'])->name('users.show');
+
+
+Route::get('/', function () {
+    return view('welcome');
+});
